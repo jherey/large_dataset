0a0e6a87545f97b5e77edcf8d3ae5d210b506fcc
Jeremiah Olufayo <olufayojeremiah@gmail.com>
2020-01-12 11:43:10 +0100

    print 432nd and 43243rd names

--- create_readstream.js
+++ create_readstream.js
@@ -10,12 +10,25 @@ const rl = readline.createInterface(inputStream, outputStream);
 // get line count for file
 let lineCount = 0;
 
+// create array list of names
+const names = [];
+
 rl.on('line', function(line) {
   // increment line count
   lineCount++;
+
+  // get all names
+  const name = line.split('|')[7];
+  if (lineCount === 431 || lineCount === 43242) {
+    names.push(name);
+  }
 });
 
 rl.on('close', function() {
   // total line count
   console.log('Total line count is: ', lineCount);
+
+  // names at various points in time
+  console.log('432nd name: ', names[0]);
+  console.log('43243rd name: ', names[1]);
 });
