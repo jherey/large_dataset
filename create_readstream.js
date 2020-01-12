6d7e2fe270896143c14a1c86aa908d0919f48c88
Jeremiah Olufayo <olufayojeremiah@gmail.com>
2020-01-12 14:26:30 +0100

    most common firstname and occurence

--- create_readstream.js
+++ create_readstream.js
@@ -3,8 +3,8 @@ const fs = require('fs');
 const readline = require('readline');
 
 const rl = readline.createInterface({
-  input: fs.createReadStream(`${__dirname}/indiv18/by_date/itcont_2018_20020411_20170529.txt`),
-  // input: fs.createReadStream(`${__dirname}/indiv18/itcont.txt`),
+  // input: fs.createReadStream(`${__dirname}/indiv18/by_date/itcont_2018_20020411_20170529.txt`),
+  input: fs.createReadStream(`${__dirname}/indiv18/itcont.txt`),
   // crlfDelay: Infinity,
 });
 
@@ -16,6 +16,9 @@ const names = [];
 
 // donations occuring in each month
 const donationsMap = new Map();
+
+// list of firstnames and most common firstnames
+const firstnameMap = new Map();
 
 rl.on('line', function(line) {
   // increment line count
@@ -33,9 +36,25 @@ rl.on('line', function(line) {
   donationsMap.has(formattedTimestamp)
     ? donationsMap.set(formattedTimestamp, donationsMap.get(formattedTimestamp) + 1)
     : donationsMap.set(formattedTimestamp, 1)
+
+  // get all firstnames
+  const firstHalfOfName = name.split(', ')[1];
+  if (firstHalfOfName !== undefined) {
+    let firstname;
+    firstHalfOfName.trim();
+
+    if (firstHalfOfName.includes(' ')) {
+      firstname = firstHalfOfName.split(' ')[0].trim();
+    } else {
+      firstname = firstHalfOfName;
+    }
+    firstnameMap.has(firstname)
+      ? firstnameMap.set(firstname, firstnameMap.get(firstname) + 1)
+      : firstnameMap.set(firstname, 1)
+  }
 });
 
-rl.on('close', function() {
+rl.on('close', () => {
   // total line count
   console.log('Total line count is: ', lineCount);
 
@@ -45,4 +64,17 @@ rl.on('close', function() {
 
   // number of donations in each month
   console.log('Number of donations: ', donationsMap);
+
+  // most common firstnames and number of times it occurs
+  let name = '', max = 0;
+  for ([key, value] of firstnameMap.entries()) {
+    if (value > max) {
+      max = value;
+      name = key;
+    }
+  }
+  console.log(
+    'Most common firstname: ', name, '\n',
+    'Number of occurence: ', max,
+  );
 });
